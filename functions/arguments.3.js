c2bc2703d81c4af4f57ac8a638d6342b16ce41ac
Jorge Rivera <jojoriveraa@gmail.com>
2018-07-23 02:52:33 -0500

    String formatter

---
+++ functions/arguments.3.js
@@ -0,0 +1,16 @@
+let add = function (a, b, c) {
+    return a + b + c
+}
+
+let result = add(10, 1, 5)
+console.log(result)
+
+let getScore = function (name = '', score = 0) {
+    return `Name: ${name} --> Score: ${score}`
+}
+
+let score = getScore('Jorge', 100)
+console.log(score)
+
+score = getScore(undefined, 90)
+console.log(score)
