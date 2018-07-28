76165b1ace898b1b8f3edd3346185214d292d569
Jorge Rivera <jojoriveraa@gmail.com>
2018-07-27 21:54:05 -0500

    for loops

--- arrays/notes.3.js
+++ arrays/notes.3.js
@@ -1,12 +1,24 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
-notes.forEach(function(){
+notes.forEach(function () {
     console.log('testing 123')
 })
 
-notes.forEach(function(item, index){
+notes.forEach(function (item, index) {
     console.log(`i: ${index} --> ${item}`)
 })
 
 console.log(notes.length)
 console.log(notes)
+
+for (let i = 0; i <= 2; i++) {
+    console.log(i)
+}
+
+for (let i = 0; i < notes.length; i++) {
+    console.log(`i: ${i} --> ${notes[i]}`)
+}
+
+for (let i = notes.length - 1; i >= 0; i--) {
+    console.log(`i: ${i} --> ${notes[i]}`)
+}
